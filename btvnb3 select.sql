1ce6fe7589bb2e11396ec6a3150634e9a529e65f
Tran Duc Loc <109507062+trducloc@users.noreply.github.com>
2023-10-30 22:55:35 +0700

    Update btvnb3 select.sql

--- btvnb3 select.sql
+++ btvnb3 select.sql
@@ -3,16 +3,22 @@ SELECT * FROM sakila.Person;
 SELECT DISTINCT job FROM Person;
 
 -- 2.Lấy ra danh sách các người có nghề nghiệp (job) là ‘Manager’ và lương (salary) lớn hơn 70000.
-SELECT * FROM Person WHERE job = 'Manager' HAVING salary > 7000;
+SELECT * FROM Person 
+WHERE job = 'Manager' 
+HAVING salary > 7000;
 
 -- 3.Lấy ra người có tuổi (dựa trên ngày sinh) từ 25 đến 35.
-SELECT * FROM Person WHERE DATE_PART('year', age(NOW(), birthdate)) BETWEEN 25 AND 35;
+SELECT * FROM Person 
+WHERE DATE_PART('year', age(NOW(), birthdate)) 
+BETWEEN 25 AND 35;
 
 -- 4.Lấy ra danh sách các quốc gia (country) và tổng số lượng người từng quốc gia.
-SELECT country, COUNT(*) AS total_people FROM Person GROUP BY country;
+SELECT country, COUNT(*) AS total_people FROM Person 
+GROUP BY country;
 
 -- 5. Tính số lượng nam (gender = ‘Male’) và nữ (gender = ‘Female’) trong bảng.
-SELECT gender, COUNT(*) AS count FROM Person GROUP BY gender;
+SELECT gender, COUNT(*) AS count FROM Person 
+GROUP BY gender;
 
 -- 6.Lấy ra danh sách người có cùng nghề nghiệp (job) và quốc gia (country).
 SELECT job, country, COUNT(*) AS count FROM Person 
